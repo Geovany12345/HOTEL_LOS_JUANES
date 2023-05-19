3e1153237bfdd8815e0ae0d58d87d63ba6fe7e43
Geovany12345 <tesis2023geo@gmail.com>
2023-05-19 17:09:38 -0500

    Add: config0000

---
+++ ProyectoHotelIU/src/java/prueba000.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Geovanny Medranda
+ */
+public class prueba000 {
+    
+}
