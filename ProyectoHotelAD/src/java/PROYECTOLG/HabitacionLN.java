abe044fa3ed3bed469868cfc173709b3807de222
Geovany12345 <tesis2023geo@gmail.com>
2023-11-09 13:38:01 -0500

    mvc actualizaciones

--- ProyectoHotelAD/src/java/PROYECTOLG/HabitacionLN.java
+++ ProyectoHotelAD/src/java/PROYECTOLG/HabitacionLN.java
@@ -1,10 +1,10 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package PROYECTOLG;
-//Alejandro Medranda
+
 import ProyectoAD.AccesoDatos;
 import ProyectoAD.HabitacionAD;
 import java.sql.ResultSet;
